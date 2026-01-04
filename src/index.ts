263209d1bf14dc14f5707cb0098bc191e053382d
Jairo <jairo.machicote@gmail.com>
2026-01-04 18:58:12 -0300

    prueba de CI/CD Back

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 //imports
 
 import express, { Request, Response } from "express";
+//import cors from 'cors';
 import "dotenv/config";
 import http, { IncomingMessage } from "http";
 import { WebSocketServer } from "ws";
